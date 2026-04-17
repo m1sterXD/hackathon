6cfcec0f07e3e8121773ca31aacd0e8fecf26382
Timothygwe <rusinshmusin@gmail.com>
2026-04-17 21:34:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import joblib
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:8080",
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", "Accept"],
+    max_age=3600,
+)
+
+model = joblib.load('')
+data = {}
+
+@app.get("/data_ars")
+async def root(data):
+    data['data_ars'] = data
+    print('was succesfully used')
+    print(data)
+    return {"message":"success"}
+
+@app.post("/predict")
+def predict(data: dict):
+    return {"prediction": model.predict([data["features"]])[0]}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
